411dc256e0e26eafeac71d06bb5c2409c7abc9bb
Görkem Yağcı <92743361+gorkemyagci@users.noreply.github.com>
2023-12-01 14:24:00 +0300

    Update music.js

--- music.js
+++ music.js
@@ -11,7 +11,7 @@ class Music {
 }
 
 const musicList = [
-    new Music("Gimme gimme", "Nuriye Teyze", '1.jpeg', '1.mp3'),
+    new Music("Gimme gimme", "Görkem", '1.jpeg', '1.mp3'),
     new Music("Başarcam Ulan", "Görkem", '2.jpeg', '3.mp3'),
     new Music("Gangsta's Paradise", "Coolio", '3.jpeg', '2.mp3')
 ];
